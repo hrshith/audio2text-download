213c3bc9b4a0da9e4ad7545514cd11ae5f5d0122
HARSHITH A S <120107362+hrshith@users.noreply.github.com>
2024-05-06 11:40:34 +0530

    Update text.py

--- text.py
+++ text.py
@@ -48,5 +48,5 @@ def download_text():
         return "No text output to download."
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
